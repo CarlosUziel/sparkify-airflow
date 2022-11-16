c77fdb7d113ad84de52238ed3ed9d517ef6635d1
cauzpema@gmail.com <cauzpema@gmail.com>
2022-11-16 22:06:28 +0100

    simplest DAG showing in Airflow UI

--- src/dags/udac_example_dag.py
+++ src/dags/udac_example_dag.py
@@ -1,15 +1,16 @@
 from datetime import datetime
 
 from airflow import DAG
-from airflow.operators import (
-    DataQualityOperator,
-    LoadDimensionOperator,
-    LoadFactOperator,
-    StageToRedshiftOperator,
-)
+from airflow.operators.empty import EmptyOperator
+
+# amazon aws provider reference:
+#   https://github.com/apache/airflow/tree/main/airflow/providers/amazon/aws
 
 # AWS_KEY = os.environ.get('AWS_KEY')
 # AWS_SECRET = os.environ.get('AWS_SECRET')
+
+# set variables/connections programatically:
+#   https://stackoverflow.com/questions/69086208/programatically-set-connections-variables-in-airflow
 
 default_args = {
     "owner": "udacity",
@@ -17,36 +18,42 @@ default_args = {
 }
 
 dag = DAG(
-    "udac_example_dag",
+    "SPARKIFY_S3_to_Redshift",
     default_args=default_args,
     description="Load and transform data in Redshift with Airflow",
     schedule_interval="0 * * * *",
 )
 
-# start_operator = DummyOperator(task_id="Begin_execution", dag=dag)
+start_operator = EmptyOperator(task_id="Begin_execution", dag=dag)
 
-stage_events_to_redshift = StageToRedshiftOperator(task_id="Stage_events", dag=dag)
 
-stage_songs_to_redshift = StageToRedshiftOperator(task_id="Stage_songs", dag=dag)
+# stage events
+# S3ToRedshiftOperator()
 
-load_songplays_table = LoadFactOperator(task_id="Load_songplays_fact_table", dag=dag)
+# stage songs
+# S3ToRedshiftOperator()
 
-load_user_dimension_table = LoadDimensionOperator(
-    task_id="Load_user_dim_table", dag=dag
-)
 
-load_song_dimension_table = LoadDimensionOperator(
-    task_id="Load_song_dim_table", dag=dag
-)
+# load_songplays_table = LoadFactOperator(task_id="Load_songplays_fact_table", dag=dag)
 
-load_artist_dimension_table = LoadDimensionOperator(
-    task_id="Load_artist_dim_table", dag=dag
-)
+# load_user_dimension_table = LoadDimensionOperator(
+#     task_id="Load_user_dim_table", dag=dag
+# )
 
-load_time_dimension_table = LoadDimensionOperator(
-    task_id="Load_time_dim_table", dag=dag
-)
+# load_song_dimension_table = LoadDimensionOperator(
+#     task_id="Load_song_dim_table", dag=dag
+# )
 
-run_quality_checks = DataQualityOperator(task_id="Run_data_quality_checks", dag=dag)
+# load_artist_dimension_table = LoadDimensionOperator(
+#     task_id="Load_artist_dim_table", dag=dag
+# )
 
-# end_operator = DummyOperator(task_id="Stop_execution", dag=dag)
+# load_time_dimension_table = LoadDimensionOperator(
+#     task_id="Load_time_dim_table", dag=dag
+# )
+
+# run_quality_checks = DataQualityOperator(task_id="Run_data_quality_checks", dag=dag)
+
+end_operator = EmptyOperator(task_id="Stop_execution", dag=dag)
+
+start_operator >> end_operator
