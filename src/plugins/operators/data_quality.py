d8ea40a761ba063a90e95f5c250e9611f4fbbb2f
cauzpema@gmail.com <cauzpema@gmail.com>
2022-11-18 07:12:30 +0100

    complete data quality operator

--- src/plugins/operators/data_quality.py
+++ src/plugins/operators/data_quality.py
@@ -1,23 +1,30 @@
+import logging
+
 from airflow.models import BaseOperator
+from airflow.providers.amazon.aws.hooks.redshift_sql import RedshiftSQLHook
 
 
 class DataQualityOperator(BaseOperator):
-
     ui_color = "#89DA59"
 
-    def __init__(
-        self,
-        # Define your operators params (with defaults) here
-        # Example:
-        # conn_id = your-connection-name
-        *args,
-        **kwargs
-    ):
-
+    def __init__(self, conn_id: str, table: str, *args, **kwargs):
         super(DataQualityOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.conn_id = conn_id
+        self.table = table
 
     def execute(self, context):
-        self.log.info("DataQualityOperator not implemented yet")
+        redshift_hook = RedshiftSQLHook(self.conn_id)
+        records = redshift_hook.get_records(f"SELECT COUNT(*) FROM {self.table}")
+        if len(records) < 1 or len(records[0]) < 1:
+            raise ValueError(
+                f"Data quality check failed. {self.table} returned no results"
+            )
+        num_records = records[0][0]
+        if num_records < 1:
+            raise ValueError(
+                f"Data quality check failed. {self.table} contained 0 rows"
+            )
+        logging.info(
+            f"Data quality on table {self.table} check passed with"
+            f" {records[0][0]} records"
+        )
